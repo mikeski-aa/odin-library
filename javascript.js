1075feacfe3228946ee199e8bdf42e6bda3f5391
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-03-05 22:09:11 +0100

    Default cards showing, toggle form, form creates
    Form now creates new cards when correctly filled.
    Form field can be hidden using the toggle button.
    5 books are showing in default cards.

--- javascript.js
+++ javascript.js
@@ -13,9 +13,28 @@ function Book(title, author, pagenum, read) {
         console.log(`Book title is ${this.title}, author is ${this.author}, length is ${this.pagenum}, is read ${read}  `)
     };
 }
-//testing the values displayed with some pre-determined values
-const book1 = new Book('The Hobbit', 'JOHN', 15, false);
-const book2 = new Book('best', 'jack', 24, true);
+//testing the values displayed with some pre-determined values, set up default cards to show
+const book1 = new Book('The Hobbit', 'J. R. R. Tolkien', 295, true);
+    addBook(book1);
+    newCard(book1);
+const book2 = new Book('The Great Gatsby', 'F Scott Fitzgerald', 194, false);
+    addBook(book2);
+    newCard(book2);
+const book3 = new Book('1984', 'George Orwell', 267, false);
+    addBook(book3);
+    newCard(book3);
+const book4 = new Book('Lolita', 'Vladimir Nabokov', 317, false);
+    addBook(book4);
+    newCard(book4);
+const book5 = new Book('One Hundred Years of Solitude', 'Gabriel Garcia Marquez', 422, false);
+    addBook(book5);
+    newCard(book5);
+
+
+    
+
+
+
 
 //function to add new book into the library
 function addBook(newBook){
@@ -85,18 +104,26 @@ function createNewBook(event) {
     let bkLngth = document.querySelector('#bLength');
     
     
-
 //if input fields are not empty, prevent default button behaviour and submit the values, afterwards reset the values
 
     if (bkTitle.value === "" || bkAuth.value === "" || bkLngth.value === "") {
         return;
     } else {
         event.preventDefault();
+
+//new book constructor
+        let readVal = false;
+        if (i === 1) {readVal = true};
+
+        let book = new Book(bkTitle.value, bkAuth.value, bkLngth.value, readVal);
+        addBook(book);
+        newCard(myLibrary[myLibrary.length - 1]);
+
         console.log(bkTitle.value);
         console.log(bkAuth.value);
         console.log(bkLngth.value);
         console.log(i);
-        rstVals(bkTitle, bkAuth, bkLngth, bkRead);
+        rstVals(bkTitle, bkAuth, bkLngth);
     }
     
 }
@@ -109,3 +136,17 @@ function rstVals(bkTitle, bkAuth, bkLngth){
     document.querySelector('#choiceOne').checked = false;
     i = 0;
 }
+
+//toggle form visibility
+addNewBookBtn.addEventListener('click', function(){
+    let inputForm = document.querySelector('form');
+    if (inputForm.style.visibility === "visible") {
+    inputForm.style.visibility = "hidden"
+    inputForm.style.height = "0";
+    addNewBookBtn.textContent = 'Click me to show form';
+} else {
+    inputForm.style.visibility = "visible"
+    inputForm.style.height = "100%";
+    addNewBookBtn.textContent = 'Click me to hide form';
+}
+})
