0596064ac0b7b70af557dd6cff9cccc870051f55
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-03-06 21:20:52 +0100

    Added formatting, readability changes
    Readability changes to javascript.js (cleaned up)
    CSS formatting added for cards, buttons

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@ let myLibrary = [];
 const container = document.querySelector('.mainContent');
 const submitBtn = document.querySelector('#submitForm')
 const addNewBookBtn = document.querySelector('.newBookForm')
+const resetBtn = document.querySelector('#resetForm')
+let bkTitle = document.querySelector('#title');
+let bkAuth = document.querySelector('#author');
+let bkLngth = document.querySelector('#bLength');
 
 let j = 0;
 
@@ -35,29 +39,60 @@ const book5 = new Book('One Hundred Years of Solitude', 'Gabriel Garcia Marquez'
 
 
     
-//function to add new book into the library
+// function to add new book into the library
 function addBook(newBook){
     myLibrary.push(newBook);
-    console.log(myLibrary);
+  
 }
 
-//function to create a new card
+// function to create a new card
+
 function newCard(book){
     let newCard = document.createElement('div');
     newCard.classList.add(`card` + j);
 
 // create new line for each part of the book object values including placeholder buttons
-    let newPone = document.createElement('p');
-    newPone.textContent = `Title: ${book.title}`;
+// very ugly solution for having 2 separate columns, which ultimately allows for two grid cols
+// could probably be shortened with the use of a function 
 
+    for (let x = 0; x < 1; x++)
+    {let newPone = document.createElement('p');
     let newPtwo = document.createElement('p');
-    newPtwo.textContent = `Author: ${book.author}`;
-
     let newPthree = document.createElement('p');
-    newPthree.textContent = `Length: ${book.pagenum}`;
+    let newPfour = document.createElement('p');
+    newPone.textContent = `Title: `;
+    newPtwo.textContent = `Author: `;
+    newPthree.textContent = `Length: `;
+    newPfour.textContent = `Read status: `;
+    newPone.classList.add('boilerField');
+    newPtwo.classList.add('boilerField');
+    newPthree.classList.add('boilerField');
+    newPfour.classList.add('boilerField');
+    newCard.appendChild(newPone);
+    newCard.appendChild(newPtwo);
+    newCard.appendChild(newPthree);
+    newCard.appendChild(newPfour);
 
+}   for (let y = 0; y < 1; y++)
+    {let newPone = document.createElement('p');
+    let newPtwo = document.createElement('p');
+    let newPthree = document.createElement('p');
     let newPfour = document.createElement('p' + j);
-    newPfour.textContent = `Read: ${book.read}`;
+    newPone.textContent = `${book.title}`;
+    newPtwo.textContent = `${book.author}`;
+    newPthree.textContent = `${book.pagenum}`;
+    if (book.read === true) {
+        newPfour.textContent = 'I have read it!'
+    } else {newPfour.textContent = `I have not read it!`}
+    newPone.classList.add('infoField1');
+    newPtwo.classList.add('infoField2');
+    newPthree.classList.add('infoField3');
+    newPfour.classList.add('boolVal');
+    newCard.appendChild(newPone);
+    newCard.appendChild(newPtwo);
+    newCard.appendChild(newPthree);
+    newCard.appendChild(newPfour);
+} 
 
     let delButton = document.createElement('button');
     delButton.classList.add('del');
@@ -65,14 +100,8 @@ function newCard(book){
 
     let readButton = document.createElement('button');
     readButton.classList.add('readToggle');
-    readButton.textContent = 'Read?'
+    readButton.textContent = 'Toggle \'read\' status';
 
-//can this be shortened?? V UGLY!
-
-    newCard.appendChild(newPone);
-    newCard.appendChild(newPtwo);
-    newCard.appendChild(newPthree);
-    newCard.appendChild(newPfour);
     newCard.appendChild(delButton);
     newCard.appendChild(readButton);
 
@@ -80,38 +109,34 @@ function newCard(book){
     return j += 1; 
 }
 
-//toggle checker for the read value
+// toggle checker for the read value
 let i = 0;
 let bkRead = document.querySelector('#choiceOne');
 bkRead.addEventListener('click', function() { 
     if (i===0){
-        console.log('Toggle On!')
         return i = 1;
     } else {
-        console.log('Toggle Off!');
         return i = 0;
     }
     })
 
-/////submit form logic
+// submit form logic
+
 submitBtn.addEventListener('click', createNewBook);
 
+// function which creates a new book in the myLibrary array
+
 function createNewBook(event) {
-    console.log('BAD!');
-    
-    let bkTitle = document.querySelector('#title');
-    let bkAuth = document.querySelector('#author');
-    let bkLngth = document.querySelector('#bLength');
-    
-    
-//if input fields are not empty, prevent default button behaviour and submit the values, afterwards reset the values
+
+// if input fields are not empty, prevent default button behaviour and submit the values, afterwards reset the values
 
     if (bkTitle.value === "" || bkAuth.value === "" || bkLngth.value === "") {
         return;
     } else {
         event.preventDefault();
 
-//new book constructor
+// new book constructor
+
         let readVal = false;
         if (i === 1) {readVal = true};
 
@@ -120,18 +145,13 @@ function createNewBook(event) {
         let book = new Book(bkTitle.value, bkAuth.value, bkLngth.value, readVal, j);
         addBook(book);
         newCard(myLibrary[myLibrary.length - 1]);
-
-        console.log(bkTitle.value);
-        console.log(bkAuth.value);
-        console.log(bkLngth.value);
-        console.log(i);
         rstVals(bkTitle, bkAuth, bkLngth);
         
     }
     
 }
 
-//reset values function
+// reset values function
 function rstVals(bkTitle, bkAuth, bkLngth){
     bkTitle.value = "";
     bkAuth.value = "";
@@ -140,7 +160,7 @@ function rstVals(bkTitle, bkAuth, bkLngth){
     i = 0;
 }
 
-//toggle form visibility
+// toggle form visibility
 addNewBookBtn.addEventListener('click', function(){
     let inputForm = document.querySelector('form');
     if (inputForm.style.visibility === "visible") {
@@ -154,77 +174,69 @@ addNewBookBtn.addEventListener('click', function(){
 }
 })
 
-//test delete button functionality, delete specified item from array
+// test delete button functionality, delete specified item from array
 container.addEventListener('click', e => {
-    console.log(e.target.parentNode.classList);
-    if (e.target.textContent === 'Delete') {
+
+    if (e.target.textContent === 'Delete'){
 
         let currcard = e.target.parentNode.classList.value;
         
-
-//seperate value to get the index of the card and array element we want to delete 
+// separate value to get the index of the card and array element we want to delete 
 // - convert newcard[1] to number fron string
 
         let newcard = currcard.split('d');
-        
         newcard[1] = +newcard[1]
-        console.log(typeof(newcard[1]));
 
-//if check to see if the item we want to delete next is before or after the item we deleted first
+// if check to see if the item we want to delete next is before or after the item we deleted first
 
         for (x of myLibrary) {
-            if (x.id == newcard[1]){
-                console.log(`The index we are deleting is:  ${myLibrary.indexOf(myLibrary[x.id])} 
-                the current ID of this index is: ${x.id}, the card number is ${newcard[1]}`);
+            if (x.id == newcard[1])
+            {
                 myLibrary = myLibrary.filter((x) => x.id !== newcard[1]);
-                
                 container.removeChild(e.target.parentNode);
-//-j counter is here to maintain correct order for new object.id values which can be created by user
-//when filling the new form
+
+// -j counter is here to maintain correct order for new object.id values which can be created by user
+// when filling the new form
 
                 j -= 1;
                 return;
             }
         } 
-    } else if (e.target.textContent === 'Read?') {
+    } else if (e.target.textContent === 'Toggle \'read\' status') {
   
         let currcard = e.target.parentNode.classList.value;
         
 
-//seperate value to get the index of the card and array element we want to delete 
-// - convert newcard[1] to number fron string
+// separate value to get the index of the card and array element we want to delete 
+// select the correct element (denoted by p + counter j when creating card elements) and changes that element value
 
         let newcard = currcard.split('d');
         let para = document.querySelector(`p${newcard[1]}`);
         newcard[1] = +newcard[1];
-        console.log(typeof(newcard[1]));
+
         for (x of myLibrary) {
             if (x.id == newcard[1]){
                 if (x.read === false) {
-                    console.log(para.textContent);
-                    para.textContent = 'Read: True'
-                    console.log('Read is False, toggle to true')
+                    para.textContent = 'I have read it!'
                     x.read = true;
                 } else {
-                    console.log(para.textContent);
-                    para.textContent = 'Read: false'
+                    para.textContent = 'I have not read it!'
                     x.read = false;
-                    console.log('Read is True, toggle to False')
                 }
                 return;
-              
-                
             }
         } 
     }
     
 
 });
-        
-        
-    // for (x of myLibrary) {
-    //     console.log(`ID of array element is ${x.id}, 
-    //     the current index of this ID is ${myLibrary.indexOf(myLibrary[x.id])}`)
-    // }
 
+// this will reset all input fields to when clicked
+resetBtn.addEventListener('click', () => {
+    bkTitle.value = "";
+    bkAuth.value = "";
+    bkLngth.value = "";
+    document.querySelector('#choiceOne').checked = false;
+    i = 0;
+})
 
